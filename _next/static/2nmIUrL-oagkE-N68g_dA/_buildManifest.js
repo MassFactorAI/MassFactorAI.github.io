71d0a5dbfe6be6d7b678f6625e10b5ec17bb116b
Henri <henri.next@bugjail.com>
2024-11-22 16:32:43 +0700

    Build from https://github.com/MassFactorAI/massfactor/commit/51edb75b994a115499fdee92dbe2e493ec386711

---
+++ _next/static/2nmIUrL-oagkE-N68g_dA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/c34c52109ae3a533.css","static/chunks/pages/index-0c89c0e5f9e6b6b5.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
